eacf8381971ab271ff394f59253eacc455fe6f2b
adjustmyself <adjust_myself@hotmail.com>
2019-11-26 09:56:43 +0800

    Updates

---
+++ precache-manifest.1a2101e21d6e7b578b165bc791043f84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ed76c49d14e010972f4306246444c7",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "d6015589897ca269dada",
+    "url": "/monster-cards/static/css/main.10326019.chunk.css"
+  },
+  {
+    "revision": "caf3d063f45c1e739e72",
+    "url": "/monster-cards/static/js/2.a68bc174.chunk.js"
+  },
+  {
+    "revision": "d6015589897ca269dada",
+    "url": "/monster-cards/static/js/main.4e12922d.chunk.js"
+  },
+  {
+    "revision": "20835d06a8b0d7694b60",
+    "url": "/monster-cards/static/js/runtime-main.daac3b9f.js"
+  }
+]);
